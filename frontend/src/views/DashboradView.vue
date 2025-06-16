d26688124f361202c69287faba4917394623f890
eldegende <eliyaz2761@gmail.com>
2025-06-16 12:38:55 +0600

    Фронтэнд, накидал логику сохранения токенов и получение профиля.

--- frontend/src/views/DashboradView.vue
+++ frontend/src/views/DashboradView.vue
@@ -1,9 +1,33 @@
 <script setup>
+import {onMounted, reactive} from "vue";
+import {getProfile} from "@/api/lib/profile.js";
+
+const state = reactive({
+  id: -1,
+  username: "",
+  createdAt: ""
+});
+
+onMounted(() => {
+  getProfile()
+      .then(res => {
+        console.log(res);
+        const { id, login, created_at } = res.data;
+        state.id = id;
+        state.username = login;
+        state.createdAt = created_at;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+});
 
 </script>
 
 <template>
   <h2>dashboard</h2>
+  <p>username: {{ state.username }}, {{ state.id }}</p>
+  <p>created at: {{ state.createdAt }}</p>
 </template>
 
 <style scoped>
